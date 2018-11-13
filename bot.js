d065788e841812bb2698cab3c8e99762d558a0ae
misslopersad <35742714+misslopersad@users.noreply.github.com>
2018-11-13 19:11:08 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,31 +1,31 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag} !`);
- 
-});
- 
+client.on("message", message => {
+var prefix = "-";
 
-var PrEfix = "-";
-client.on('message', message => {
-  if (!message.content.startsWith(PrEfix)) return;
-  var args = message.content.split(' ').slice(1);
-  var argresult = args.join(' ');
-  if (message.author.id !== '426744069163843584') return;
-if (message.content.startsWith(PrEfix + 'wt')) {
-client.user.setActivity(argresult, {type:'WATCHING'});
-    message.channel.sendMessage(`**✅  : ${argresult}**`)
-} else 
-
-if (message.content.startsWith(PrEfix + 'ls')) {
-client.user.setActivity(argresult, {type:'LISTENING'});
-    message.channel.sendMessage(`**✅  : ${argresult}**`)
-} else 
-if (message.content.startsWith(PrEfix + 'st')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/amira");
-    message.channel.sendMessage(`**✅  : ${argresult}**`)
-}
+    var command = message.content.split(" ")[0];
+    command = command.slice(prefix.length);
+        let args = message.content.split(" ").slice(1).join(" ");
+            if(message.author.bot) return;
+                if(!message.channel.guild) return;
+                    if(!message.guild.member(message.author.id).hasPermission("ADMINISTRATOR")) return;
+                if(!args) return;
+    if(command === "bc") {
+        message.channel.send("**Sent.**").then(m => m.delete(5000));
+        message.guild.members.forEach(member => {
+            let msg = args.replace("[user]", member);
+                member.send(msg);
+        });
+    }
+    if(command === "obc") {
+        message.channel.send("**Sent.**").then(m => m.delete(5000));
+        message.guild.members.filter(a => a.user.presence.status !== "offline").forEach(member => {
+            let msg = args.replace("[user]", member);
+            member.send(msg);
+        });
+    }
+    
 
 });
 
